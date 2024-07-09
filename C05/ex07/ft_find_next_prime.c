ba2295a366fd13242db6e789a3a2792137942d2a
Abdelkarim Labib <ablabib@e2r8p9.1337.ma>
2024-07-09 09:23:00 +0100

    C05-new-v

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablabib <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 09:20:00 by ablabib           #+#    #+#             */
+/*   Updated: 2024/07/09 09:20:01 by ablabib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (ft_is_prime(nb) == 1)
+	{
+		return (nb);
+	}
+	while (nb)
+	{
+		nb++;
+		if (ft_is_prime(nb) == 1)
+			return (nb);
+	}
+}
