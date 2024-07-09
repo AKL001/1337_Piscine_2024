1158deafe3278c13965ff2d643a139be6b6b4b76
Abdelkarim Labib <ablabib@e2r7p6.1337.ma>
2024-07-09 21:59:42 +0100

    needTesting

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablabib <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 21:40:41 by ablabib           #+#    #+#             */
+/*   Updated: 2024/07/09 21:57:10 by ablabib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((*s1 && *s2) && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	write(1, str, i);
+	write(1, "\n", 1);
+}
+
+void	_sort(char *arr[], int n)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	while (i < n - 1)
+	{
+		j = i + 1;
+		while (j < n)
+		{
+			if (ft_strcmp(arr[i], arr[j]) > 0)
+			{
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int arc, char *str[])
+{
+	char	*tmp;
+	int		i;
+	int		j;
+
+	if (arc < 2)
+		return (0);
+	_sort(str, arc);
+	i = 1;
+	while (i < arc)
+	{
+		ft_putstr(str[i]);
+		i++;
+	}
+}
